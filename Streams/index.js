17a94207a09888a96bf8f285a3dd3475abd4f098
Prince Chaurasia <princechaurasiaofficial24@gmail.com>
2023-04-27 14:28:52 +0530

    adding a easy method to steam at line 46-47

--- Streams/index.js
+++ Streams/index.js
@@ -27,19 +27,27 @@ server.on("request", (req, res) => {
   //     if(err) return console.error(err)
   //     res.end(data.toString())
   // })
+//   const rstream = fs.createReadStream("input.txt");
+
+//   rstream.on("data", (chunkData) => {
+//     res.write(chunkData);
+//   });
+
+//   rstream.on("end", () => {
+//     res.end();
+//   });
+//   rstream.on("error", (err)=>{
+//     console.log(err);
+//     res.end("file not found")
+//   })
+
+  // one line code for the same function to be done 
+
   const rstream = fs.createReadStream("input.txt");
+  rstream.pipe(res);
 
-  rstream.on("data", (chunkData) => {
-    res.write(chunkData);
-  });
+  // -------------------------------->
 
-  rstream.on("end", () => {
-    res.end();
-  });
-  rstream.on("error", (err)=>{
-    console.log(err);
-    res.end("file not found")
-  })
 });
 
 server.listen(8000, "127.0.0.1")
